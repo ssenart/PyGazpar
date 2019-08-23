5dff9c4a94de43281521b5238c421c07ff5563d6
Stéphane Senart <stephane.senart@gmail.com>
2019-08-23 14:59:07 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from distutils.core import setup
+import setuptools
 setup(
   name = 'pygazpar',         # How you named your package folder (MyLib)
   packages = ['pygazpar'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Retrieve gas consumption from GrDF web site (French Gas Company)',   # Give a short description about your library
-  author = 'Stéphane Senart',                   # Type in your name
+  author = 'Stephane Senart',                   # Type in your name
   author_email = 'stephane.senart@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ssenart/PyGazpar',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/ssenart/PyGazpar/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['ENERGY', 'GAS', 'CONSUMPTION'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'selenium',
-          'time',
-          'openpyxl',
-          'os',
-          'glob'
+          'selenium == 3.141',
+          'openpyxl == 2.6.3'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    'Intended Audience :: Developers',      # Define that your audience are developers

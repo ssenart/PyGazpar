c5ff5e92def2046a30c2619cb241baac76a6000a
Stéphane Senart <stephane.senart@gmail.com>
2019-08-25 23:25:04 +0200

    Close WebDriver even in case of exceptions

--- pygazpar/client.py
+++ pygazpar/client.py
@@ -41,68 +41,70 @@ class Client(object):
         profile.set_preference('browser.download.dir', self.__tmp_directory)
         profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         
-        driver = webdriver.Firefox(executable_path=self.__firefox_webdriver_executable, firefox_profile=profile, options=options, service_log_path=self.__tmp_directory + '/geckodriver.log')
-        driver.set_window_position(0, 0)
-        driver.set_window_size(1200, 1200)
-        
-        driver.implicitly_wait(10)
-        
-        driver.get(HOME_URL)
-        
-        # Fill login form
-        email_element = driver.find_element_by_id("_EspacePerso_WAR_EPportlet_:seConnecterForm:email")
-        password_element = driver.find_element_by_id("_EspacePerso_WAR_EPportlet_:seConnecterForm:passwordSecretSeConnecter")
-        
-        email_element.send_keys(self.__username)
-        password_element.send_keys(self.__password)
-        
-        submit_button_element = driver.find_element_by_id('_EspacePerso_WAR_EPportlet_:seConnecterForm:meConnecter')
-        submit_button_element.click()
-        
-        # Wait a few for the login to complete
-        time.sleep(10)
-        
-        # Page 'votre consomation'
-        driver.get(DATA_URL)
+        try:
+            driver = webdriver.Firefox(executable_path=self.__firefox_webdriver_executable, firefox_profile=profile, options=options, service_log_path=self.__tmp_directory + '/geckodriver.log')
+            driver.set_window_position(0, 0)
+            driver.set_window_size(1200, 1200)
+            
+            driver.implicitly_wait(10)
+            
+            driver.get(HOME_URL)
+            
+            # Fill login form
+            email_element = driver.find_element_by_id("_EspacePerso_WAR_EPportlet_:seConnecterForm:email")
+            password_element = driver.find_element_by_id("_EspacePerso_WAR_EPportlet_:seConnecterForm:passwordSecretSeConnecter")
+            
+            email_element.send_keys(self.__username)
+            password_element.send_keys(self.__password)
+            
+            submit_button_element = driver.find_element_by_id('_EspacePerso_WAR_EPportlet_:seConnecterForm:meConnecter')
+            submit_button_element.click()
+            
+            # Wait a few for the login to complete
+            time.sleep(10)
+            
+            # Page 'votre consomation'
+            driver.get(DATA_URL)
 
-        # Wait a few for the data page load to complete
-        time.sleep(10)
-        
-        # Close Popup Windows
-        close_popup_element = driver.find_elements_by_css_selector('.abtasty-modal__close > svg')
-        close_popup_element[0].click()
-        time.sleep(5)
+            # Wait a few for the data page load to complete
+            time.sleep(10)
+            
+            # Close Popup Windows
+            close_popup_element = driver.find_elements_by_css_selector('.abtasty-modal__close > svg')
+            close_popup_element[0].click()
+            time.sleep(5)
 
-        # Select daily consumption
-        daily_consumption_element = driver.find_element_by_xpath("//table[@id='_eConsoconsoDetaille_WAR_eConsoportlet_:idFormConsoDetaille:panelTypeGranularite1']/tbody/tr/td[3]/label")
-        daily_consumption_element.click()
-        time.sleep(5)
+            # Select daily consumption
+            daily_consumption_element = driver.find_element_by_xpath("//table[@id='_eConsoconsoDetaille_WAR_eConsoportlet_:idFormConsoDetaille:panelTypeGranularite1']/tbody/tr/td[3]/label")
+            daily_consumption_element.click()
+            time.sleep(5)
 
-        # Download file
-        download_button_element = driver.find_element_by_xpath("//button[@id='_eConsoconsoDetaille_WAR_eConsoportlet_:idFormConsoDetaille:telechargerDonnees']/span")
-        download_button_element.click()
-        
-        # Wait a few for the download to complete
-        time.sleep(10)
-        
-        # Close the driver
-        driver.close()
-        
-        # Load the XLSX file into the data structure
-        file_list = glob.glob(data_file_path_pattern)
+            # Download file
+            download_button_element = driver.find_element_by_xpath("//button[@id='_eConsoconsoDetaille_WAR_eConsoportlet_:idFormConsoDetaille:telechargerDonnees']/span")
+            download_button_element.click()
+            
+            # Wait a few for the download to complete
+            time.sleep(10)
+            
+            # Load the XLSX file into the data structure
+            file_list = glob.glob(data_file_path_pattern)
 
-        for filename in file_list:
-            wb = load_workbook(filename = filename)
-            ws = wb['Historique par jour']
-            for rownum in range(8, 365):
-                row = {}
-                if ws.cell(column=2, row=rownum).value != None:
-                    row['time'] = ws.cell(column=2, row=rownum).value
-                    row['total_m3'] = ws.cell(column=4, row=rownum).value
-                    row['total_kWh'] = float(ws.cell(column=4, row=rownum).value) * 11.244
-                    row['daily_m3'] = ws.cell(column=5, row=rownum).value
-                    row['daily_kWh'] = ws.cell(column=6, row=rownum).value
-                    self.data.append(row)
-            wb.close()
-        
-            os.remove(filename)
+            for filename in file_list:
+                wb = load_workbook(filename = filename)
+                ws = wb['Historique par jour']
+                for rownum in range(8, 365):
+                    row = {}
+                    if ws.cell(column=2, row=rownum).value != None:
+                        row['time'] = ws.cell(column=2, row=rownum).value
+                        row['total_m3'] = ws.cell(column=4, row=rownum).value
+                        row['total_kWh'] = float(ws.cell(column=4, row=rownum).value) * 11.244
+                        row['daily_m3'] = ws.cell(column=5, row=rownum).value
+                        row['daily_kWh'] = ws.cell(column=6, row=rownum).value
+                        self.data.append(row)
+                wb.close()
+            
+                os.remove(filename)
+
+        finally:
+            # Close the driver
+            driver.close()

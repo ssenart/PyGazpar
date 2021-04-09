47dcb30093c7906f88c821c1eaa2bd8f07c958d9
Stéphane Senart <stephane.senart@gmail.com>
2021-04-09 11:55:17 +0200

    Improve CI/CD workflow.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,7 @@
 from pygazpar.client import Client
+import pytest
 
-
-class ClientTestCase:
+class TestClient:
 
     username = ""
     password = ""
@@ -9,8 +9,12 @@ class ClientTestCase:
     wait_time = 30
     tmp_directory = ""
 
+    @pytest.mark.skip(reason="Requires credentials")
     def test_client(self):
         client = Client(self.username, self.password, self.webdriver, self.wait_time, self.tmp_directory)
         client.update()
 
         assert len(client.data()) != 0
+
+    def test_empty(self):
+        assert 1 == 1

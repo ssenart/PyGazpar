e19461853bba8aa2d3f7d3980016359774861520
Stéphane Senart <stephane.senart@gmail.com>
2022-12-11 17:47:33 +0100

    [#60] [Feature] Permit to load data at multiple frequencies with one method call

--- pygazpar/enum.py
+++ pygazpar/enum.py
@@ -16,6 +16,9 @@ class PropertyName(Enum):
     def __str__(self):
         return self.value
 
+    def __repr__(self):
+        return self.__str__()
+
 
 # ------------------------------------------------------------------------------------------------------------
 class Frequency(Enum):
@@ -27,3 +30,6 @@ class Frequency(Enum):
 
     def __str__(self):
         return self.value
+
+    def __repr__(self):
+        return self.__str__()
